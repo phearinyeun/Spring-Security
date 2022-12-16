879150be583709002d595a71723d4a1dbc86512b
Loranyan <phearinyeun2019@gmail.com>
2022-12-16 18:17:42 +0700

    upload

---
+++ src/test/java/Spring/Security/Spring/Security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.Security.Spring.Security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
